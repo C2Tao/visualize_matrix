446d73008ec44dfa96382cd53d65c013e095d951
c2tao <b97901182@gmail.com>
2017-02-19 03:44:21 +0800

    sparsified and clustered matrix

--- Vmatrix.py
+++ Vmatrix.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+from itertools import ifilterfalse
+import string
+import matplotlib.pyplot as plt
 
 def reorder_center(n):
     order = []
@@ -16,6 +18,25 @@ def reorder_center(n):
             order.append(m+i+1)
     assert len(order)==n
     return np.array(order)
+
+def unique_everseen(iterable, key=None):
+    "List unique elements, preserving order. Remember all elements ever seen."
+    # unique_everseen('AAAABBBCCDAABBB') --> A B C D
+    # unique_everseen('ABBCcAD', str.lower) --> A B C D
+    seen = set()
+    seen_add = seen.add
+    if key is None:
+        for element in ifilterfalse(seen.__contains__, iterable):
+            seen_add(element)
+            yield element
+    else:
+        for element in iterable:
+            k = key(element)
+            if k not in seen:
+                seen_add(k)
+                yield element
+
+
 
 class Vmatrix(object):
     def __init__(self, mat, col=None, row=None):
@@ -39,31 +60,78 @@ class Vmatrix(object):
     
     def mat_sparsify(self, thresh=None):
         # set all values of the matrix to be 1 if >= thresh, else 0
+        # remove rows/cols if all 0
         sparse_mat = np.where(self.mat>=thresh, 1, 0)
-        return Vmatrix(sparse_mat, self.col, self.row)
+        return Vmatrix(sparse_mat, self.col, self.row).mat_thresh(1)
      
-    def mat_col(self, col):
-        # make a submatrix by keep only the selected columns
-        return Vmatrix(self.mat[col,:], self.col[col], self.row)
+    def mat_col_idx(self, idx_col):
+        # make a submatrix by keep only the selected columns indices
+        return Vmatrix(self.mat[idx_col,:], self.col[idx_col], self.row)
+    
+    def mat_col_key(self, key_col):
+        # make a submatrix by keep only the selected columns keys
+        idx_col = [list(self.col).index(k) for k in key_col] 
+        return Vmatrix(self.mat[idx_col,:], key_col, self.row)
 
     def col_thresh(self, thresh = None):
         # remove rows where all values are smaller than thresh
-        # this means keeping only a sublist of original col indices
+        # this means keeping only a sublist of original cols
         keep_col = np.invert(np.all(self.mat < thresh, axis=1, keepdims=False)).nonzero()[0]
-        return self.mat_col(keep_col)
+        return self.mat_col_idx(keep_col)
 
     def col_sort(self):
-        # sort columns in ascending order according to row sum
-        sorted_col = sorted(range(self.mat.shape[0]), key= lambda i: np.sum(self.mat[i,:])) 
-        return self.mat_col(sorted_col)
+        # sort columns in descending order according to row sum
+        sorted_col = sorted(range(self.mat.shape[0]), key= lambda i: np.sum(self.mat[i,:]), reverse=True) 
+        return self.mat_col_idx(sorted_col)
+
+    #def row_sort(self):
+    #    # sort rows in ascending order according to col sum
+    #    return self.mat_transpose().col_sort().mat_transpose()
+
+    def col_cluster(self, thresh):
+        # 1. X = sparsifies matrix
+        # 2. sorted_row = sorted row of sparsified matrix
+        # 3. clustered_col = cluster columns together according to row priority
+        # 4. new_row/col_order = reorder_center(sorted_row/clustered_col)
+        # returns X.mat[#centered(clustered_col), centered(sorted_row)]
+
+        ### sparsify matrix
+        #print 'sparsify'
+        S = self.mat_sparsify(thresh)#.debug()
         
-    def debug(self, max_display=6):
+        ### sort rows
+        #print 'sort row'
+        V = S.mat_transpose().col_sort().mat_transpose()#.debug()
+        sorted_row = V.row
+        
+        ### cluster columns
+        #print 'cluster col'
+        clustered_col = []
+        for i, row in enumerate(sorted_row):
+            for j, val in enumerate(V.mat[:, i]):
+                if val!=0: 
+                    clustered_col.append(V.col[j])
+        #print clustered_col 
+
+        clustered_col = np.array(list(unique_everseen(clustered_col)))
+        
+        #return self.mat_col_key(clustered_col).mat_transpose().mat_col_key(sorted_row).mat_transpose()
+        return V.mat_col_key(clustered_col)
+
+    def view(self):
+        plt.matshow(self.mat)
+        plt.show()
+        
+        return self 
+
+    def debug(self, max_display=10):
         print 'mat_shape', self.mat.shape
         print 'col ', self.col
         print 'row ', self.row
         #print '    ', self.row
         for i, row in enumerate(self.mat[:max_display]):
             print '   {}'.format(self.col[i]), row
+        
         return self
 
 
@@ -87,6 +155,11 @@ def test_case():
     print '================================================================'
 
 
-
 if __name__=='__main__':
-    test_case()
+    #test_case()
+    alphabets = list(string.ascii_lowercase)
+    print alphabets
+    V = Vmatrix(np.random.rand(10,10), col=alphabets[:10]).debug().view()
+    G = V.col_cluster(0.8).debug().view()
+
+    

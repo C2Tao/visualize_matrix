f5a84c5f590b0d32a68130bf74780a87b6c0df3b
c2tao <b97901182@gmail.com>
2017-02-19 05:39:01 +0800

    timit result: female vs male speaker

--- Vmatrix.py
+++ Vmatrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+from zrst.util import MLF
 from itertools import ifilterfalse
 import string
 import matplotlib.pyplot as plt
@@ -100,6 +101,18 @@ class Vmatrix(object):
     #    # sort rows in ascending order according to col sum
     #    return self.T().col_sort().T()
 
+    def col_aggr(self, sorted_row):
+        clustered_col = []
+        for i, row in enumerate(sorted_row):
+            for j, val in enumerate(V.mat[:, i]):
+                if val!=0: 
+                    clustered_col.append(V.col[j])
+        clustered_col = np.array(list(unique_everseen(clustered_col)))
+
+    def col_center(self):
+        new_col = array_center(self.col)
+        return self.mat_col_key(new_col)
+
     def col_cluster(self, thresh):
         # 1. X = sparsified matrix
         # 2. sorted_row = sorted row of sparsified matrix
@@ -126,11 +139,13 @@ class Vmatrix(object):
         clustered_col = np.array(list(unique_everseen(clustered_col)))
         #print clustered_col 
 
-        new_col = array_center(clustered_col)
-        new_row = array_center(sorted_row)
+        new_col = clustered_col
+        new_row = sorted_row
+        #new_col = array_center(clustered_col)
+        #new_row = array_center(sorted_row)
         
-        #return self.mat_col_key(new_col).T().mat_col_key(new_row).T()
-        return S.mat_col_key(new_col).T().mat_col_key(new_row).T()
+        return self.mat_col_key(new_col).T().mat_col_key(new_row).T()
+        #return S.mat_col_key(new_col).T().mat_col_key(new_row).T()
 
     def view(self):
         plt.matshow(self.mat)
@@ -139,12 +154,12 @@ class Vmatrix(object):
         return self 
 
     def debug(self, max_display=10):
-        print 'mat_shape', self.mat.shape
-        print 'col ', self.col
-        print 'row ', self.row
-        #print '    ', self.row
+        print '    ', self.row
         for i, row in enumerate(self.mat[:max_display]):
             print '   {}'.format(self.col[i]), row
+        print 'col ', self.col
+        print 'row ', self.row
+        print 'mat_shape', self.mat.shape
         
         return self
 
@@ -168,14 +183,98 @@ def test_case():
     L = V.mat_sparsify(4).debug()
     print '================================================================'
 
-
-if __name__=='__main__':
-    #test_case()
+def test_rand():
     alphabets = list(string.ascii_lowercase)
     print alphabets
     print array_center(alphabets)
     #V = Vmatrix(np.random.rand(10,10), col=alphabets[:10]).debug().view()
     V = Vmatrix(np.random.rand(100,100)).debug().view()
     G = V.col_cluster(0.8).debug().view()
+
+
+
+def vector_occr(word_list, vocab_list):
+    return np.array(map(lambda x: word_list.count(x), vocab_list),dtype=np.float32)
+
+def matrix_from_mlf(mlf_path):
+    mlf = MLF(mlf_path)
+    speaker_list, vector_list = [], []
+    vocab_list = mlf.tok_list
+    for i, tags in enumerate(mlf.tag_list):
+        speaker = '_'.join(mlf.wav_list[i].split('_')[1:3])
+        vector = vector_occr(tags, vocab_list)
+        if i!=0 and speaker_list[-1]==speaker:
+            vector_list[-1]+=vector
+        else:
+            #if i!=0: print speaker_list[-1], vector_list[-1]
+            speaker_list.append(speaker)
+            vector_list.append(vector)
+    matrix = np.array(vector_list)
+    return matrix, speaker_list, vocab_list
+
+def matrix_accu_attr(mat, speaker_list, attr):
+    if attr=='gender':
+        idx = 4
+    elif attr=='region':
+        idx = 2
+    attr_list = sorted(list(set(map(lambda sp: sp[idx], speaker_list))))
+    attr_speaker = [[] for ___ in attr_list]
+    attr_idx = {attr: attr_list.index(attr) for attr in attr_list}
+    matrix = np.zeros((len(attr_list), mat.shape[1]), dtype=np.float32)
+    for i, sp in enumerate(speaker_list):
+        j = attr_idx[sp[idx]]
+        attr_speaker[j].append(sp)
+        matrix[j, :] +=mat[i,:]
+
+    for i, a in enumerate(attr_list):
+        print a,len(attr_speaker[i])
+    flatten = lambda l: [item for sublist in l for item in sublist]
+    return matrix, attr_list, flatten(attr_speaker)
+
+
+def current_best():
+    mlf_path = '/mnt/c/Users/c2tao/Desktop/Semester 18/tokenizer_bnf0_mr1/500_5/result/result.mlf'
+    SV, spk_list, v_list = matrix_from_mlf(mlf_path)
+    gSV, gen_list = matrix_accu_attr(SV, spk_list, 'gender')
+    rSV, reg_list = matrix_accu_attr(SV, spk_list, 'region')
+    #plt.matshow(np.log(SV+1))
+    #plt.show()
+    
+    V = Vmatrix(SV, spk_list, v_list)#.view()
+    #V = Vmatrix(rSV, reg_list, v_list)#.view()
+
+    #X = V.mat_thresh(10)#.view()
+    sorted_rows = V.T().col_sort().T().row[20:50]
+    #sorted_cols = X.col_sort().col
+
+    #V = V.mat_col_key(sorted_cols).col_center()#.view()
+    V = V.T().mat_col_key(sorted_rows).col_center().T().view()
+
+ 
+    #X = V.mat_sparsify(20)
+    #V.col_cluster(20).debug().view()
+
+if __name__=='__main__':
+    mlf_path = '/mnt/c/Users/c2tao/Desktop/Semester 18/tokenizer_bnf0_mr1/50_3/result/result.mlf'
+    SV, spk_list, v_list = matrix_from_mlf(mlf_path)
+    gSV, gen_list, gspk_list = matrix_accu_attr(SV, spk_list, 'gender')
+    rSV, reg_list, rspk_list = matrix_accu_attr(SV, spk_list, 'region')
+    #plt.matshow(np.log(SV+1))
+    #plt.show()
+    
+    V = Vmatrix(SV, spk_list, v_list)#.view()
+    V = V.mat_col_key(gspk_list)#.debug()
+    #V = Vmatrix(rSV, reg_list, v_list)#.view()
+
+    #X = V.mat_thresh(10)#.view()
+    sorted_rows = V.T().col_sort().T().row[20:200]
+    #sorted_cols = X.col_sort().col
+
+    #V = V.mat_col_key(sorted_cols).col_center()#.view()
+    V = V.T().mat_col_key(sorted_rows).col_center().T().view()
+    #V.mat_sparsify(3).view().debug()
+    #X = V.mat_sparsify(20)
+    #V.col_cluster(20).debug().view()
+
 
     
